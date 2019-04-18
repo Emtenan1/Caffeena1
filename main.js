6c896a202b80a9799caf26db7131c4182e3608ce
Emtinan Alnasser <emtinanalnasser@MacBook-Pro-alkhas-b-Emtinan.local>
2019-04-18 09:31:40 +0300

    Final Project

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-<button on
+$(document).ready(function(){
+    console.log($('.h1'))
+
+    $('.h1').on('click', aboutus);
+
+    function aboutus(){
+        console.log('click')
+        $('.h1').toggleClass('top');
+        $('#AU').fadeToggle();
+    }
+
+    $('.Hellopage').on('click',hello);
+    function hello(){
+        window.location.href = "Index.html"
+    }
+    
+    $('#menu').on('click',menu);
+    function menu(){
+        $('.menu1').slideToggle()
+    }
+});
+
